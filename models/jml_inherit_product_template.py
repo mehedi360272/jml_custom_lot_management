969f0a1b94894522199f5313db19d51e2d25a326
mehedi360272 <mehedi360272@gmail.com>
2025-05-20 10:37:54 +0600

    updated

--- models/jml_inherit_product_template.py
+++ models/jml_inherit_product_template.py
@@ -16,3 +16,32 @@ class ProductProduct(models.Model):
     avg_weight = fields.Float(string="Average Weight")
 
     lock_product = fields.Boolean(string='Lock Product')
+
+    lot_count = fields.Integer(
+        string='Lot Count',
+        compute='_compute_lot_count'
+    )
+
+    @api.depends('product_variant_ids')
+    def _compute_lot_count(self):
+        for template in self:
+            products = template.product_variant_ids
+            lots = self.env['stock.lot'].search([
+                ('product_id', 'in', products.ids),
+                ('product_qty', '>', 0)
+            ])
+            template.lot_count = len(lots)
+
+    def action_view_lots(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Lots',
+            'res_model': 'stock.lot',
+            'view_mode': 'list,form',
+            'domain': [
+                ('product_id', 'in', self.product_variant_ids.ids),
+                ('product_qty', '>', 0),
+            ],
+            'context': {'search_default_group_by_product_id': 1},
+        }
